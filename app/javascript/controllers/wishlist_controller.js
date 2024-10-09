475905b207f9e68dcaea27868a06014869aa88df
Sajib Saha <sajib.saha@brainstation-23.com>
2024-10-09 17:24:48 +0600

    Wishlist added

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -2,14 +2,69 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   updateWishlistStatus() {
-    if(this.element.dataset.status === "false") {
-        this.element.classList.remove("fill-none")
-        this.element.classList.add("fill-primary")
-        this.element.dataset.status = "true"
-    } else {
-        this.element.classList.remove("fill-primary")
-        this.element.classList.add("fill-none")
-        this.element.dataset.status = "false"
+    if (this.element.dataset.status === "false"){
+        const propertyId = this.element.dataset.propertyId;
+        const userId = this.element.dataset.userId;
+        this.addPropertyToWishlist(propertyId, userId)
     }
+    else {
+        const wishlistId = this.element.dataset.wishlistId;
+        this.removePropertyFromWishlist(wishlistId)
+    }
+  }
+
+  addPropertyToWishlist(propertyId, userId){
+    const params = {
+      property_id: propertyId,
+      user_id: userId,
+    };
+      
+    const options = {
+      method: 'POST',
+      headers: {
+      'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    };
+
+    fetch('/api/wishlists', options)
+    .then(response => {
+      if (!response.ok) {
+        console.log(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      this.element.dataset.wishlistId = data.id;
+      this.element.dataset.status = "true";
+      this.element.classList.remove("fill-none");
+      this.element.classList.add("fill-primary");
+      
+      if (this.textTarget) {
+        this.textTarget.innerText = 'Saved';
+      }
+    })
+    .catch(e => {
+      console.log(e);
+    });
+  }
+
+  removePropertyFromWishlist(wishlistId) {
+    fetch('/api/wishlists/' + wishlistId, {
+      method: 'DELETE',
+    })
+    .then(response => {
+      this.element.dataset.wishlistId = '';
+      this.element.dataset.status = "false";
+      this.element.classList.remove("fill-primary");
+      this.element.classList.add("fill-none");
+
+      if (this.textTarget) {
+        this.textTarget.innerText = 'Save';
+      }
+    })
+    .catch(e => {
+      console.log(e);
+    });
   }
 }
